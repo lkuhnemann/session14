d3fee5007986ffa8c755269a47e319a7d250fa96
larakuehnemann <lkuhnemann.ieu2023@student.ie.edu>
2025-02-21 13:14:55 +0100

    countries

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import china, austria
+from austria import cook as austria_cook
+from china import cook as china_cook
+from latam.argentina import cook as argentina_cook
+from latam.brazil import cook as brazil_cook
+from latam.mexico.yucatan import cook as yucatan_cook
+
+
+def cook():
+    print("we are making paella")
+
+print("a random number is: ", random.randint(1,10))
+china.cook()
+china.greet()
+austria.cook()
+austria.greet()
+cook()
+argentina_cook()
+brazil_cook()
+yucatan_cook()
+
+
+
+
